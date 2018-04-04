9238290b9936b7a9bd0a17de574231fe74cc7492
janvanrijn <janvanrijn@gmail.com>
2018-04-04 13:59:08 -0400

    adapted cortana to evaluation

--- OpenmlCortana/src/org/openml/cortana/utils/Evaluations.java
+++ OpenmlCortana/src/org/openml/cortana/utils/Evaluations.java
@@ -32,10 +32,10 @@ public class Evaluations {
 			arrayDataPositives.add((int) Double.parseDouble(csvRecord.get(5)));
 		}
 		
-		scores.add(new EvaluationScore("coverage", arrayDataCoverage.get(0) + "", null, arrayDataCoverage.toString()));
-		scores.add(new EvaluationScore("probability", arrayDataProbability.get(0) + "", null, arrayDataProbability.toString()));
-		scores.add(new EvaluationScore("positives", arrayDataPositives.get(0) + "", null, arrayDataPositives.toString()));
-		scores.add(new EvaluationScore(nameMapping(measure), arrayDataQuality.get(0) + "", null, arrayDataQuality.toString()));
+		scores.add(new EvaluationScore("coverage", arrayDataCoverage.get(0) * 1.0, null, arrayDataCoverage.toString()));
+		scores.add(new EvaluationScore("probability", arrayDataProbability.get(0), null, arrayDataProbability.toString()));
+		scores.add(new EvaluationScore("positives", arrayDataPositives.get(0) * 1.0, null, arrayDataPositives.toString()));
+		scores.add(new EvaluationScore(nameMapping(measure), arrayDataQuality.get(0), null, arrayDataQuality.toString()));
 		
 		return scores;
 	}
